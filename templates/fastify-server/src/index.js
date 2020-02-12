9e9c3926f7bfd01c619121f2cfcf535a6b42aea8
Ivan Malkov <ozonep@yandex.ru>
2020-02-12 16:06:35 +0300

    Fixed typo and working on plugin for Fastify

--- templates/fastify-server/src/index.js
+++ templates/fastify-server/src/index.js
@@ -10,16 +10,17 @@ const fastify = Fastify();
 fastify.register(fastifyLoader, {
     paths: ['./routes/*.js']
 });
+//fastify.register(require('./routes/user'))
 fastify.register(fastifyFormbody);
 fastify.register(fastifyMultipart);
 fastify.register(fastifyCors, {});
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', (err, address) => {
+fastify.listen(port, '0.0.0.0', (err, address) => {
     if (err) {
-        app.log.error(err);
+        fastify.log.error(err);
         process.exit(1)
     }
-    app.log.info(`server listening on ${address}`)
+    fastify.log.info(`server listening on ${address}`)
 });
---
+++ templates/fastify-server/src/plugin/inject.js
@@ -0,0 +1,42 @@
+const fg = require('fast-glob');
+const path = require('path');
+const {NodeVM} = require('vm2');
+const fs = require('fs');
+const debug = require('debug')('inject');
+const builtinModules = require('builtin-modules');
+
+/**
+ *
+ * @param {String[]} globs
+ * @param {Object<String, Object>} toInject
+ */
+function inject(globs, toInject, parent) {
+    debug(`Parent filename: ${parent}`);
+
+    const paths = fg.sync(globs, {
+        cwd: path.dirname(parent)
+    }).map(p => path.join(path.dirname(parent), p));
+
+    debug(`Transformed Globs: ${paths}`);
+
+    paths.map(p => {
+        debug(`Create VM for ${p}`);
+        const vm = new NodeVM({
+            sandbox: toInject,
+            nesting: true,
+            require: {
+                external: true,
+                builtin: builtinModules
+            },
+            wrapper: 'commonjs'
+        });
+        const code = fs.readFileSync(p, { encoding: 'utf8' });
+        debug(`Code for ${p}: ${code}`);
+        vm.run(code, p);
+        debug(`Executed ${p}`);
+    });
+}
+
+module.exports = {
+    requireAndInject: inject
+};
