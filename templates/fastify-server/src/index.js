5bb8fdda6507f00993e7e6fe2c417ae94ec53dcc
Ivan Malkov <ozonep@yandex.ru>
2020-03-04 13:41:49 +0300

    Replace built-in ajv validator with ajv-oai for int32/64 support

--- templates/fastify-server/src/index.js
+++ templates/fastify-server/src/index.js
@@ -2,8 +2,18 @@ import Fastify from 'fastify';
 import fastifyFormbody from 'fastify-formbody';
 import fastifyMultipart from 'fastify-multipart';
 import fastifyCors from 'fastify-cors';
+import Ajv from "ajv-oai";
 
 const fastify = Fastify();
+
+const ajv = new Ajv({
+    removeAdditional: true,
+    useDefaults: true,
+    coerceTypes: true,
+    nullable: true
+});
+
+fastify.setSchemaCompiler(schema => ajv.compile(schema));
 
 
 fastify.register(fastifyFormbody);
